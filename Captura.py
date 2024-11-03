c11ac257584d6124ca2543f5259b15db1e5f50e2
Paulo Andé <45316554+PauloAndr@users.noreply.github.com>
2024-11-03 10:51:44 -0300

    Tempo de bit Att
    agora para receber um bit válido vai ser esperado uma sequencia de bits como tempo de bit

--- Captura.py
+++ Captura.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 
+
 def capture():
     cap = cv2.VideoCapture(0)
 
@@ -8,8 +9,13 @@ def capture():
         print("Erro ao acessar a câmera")
         return
 
+    bits_validos = [0, 0, 0, 0, 0, 0, 0, 0] # Inicializa a lista para armazenar os bits válidos
+    p = 0
+    tempo_bit = [0, 0, 0, 0]  # Inicializa a lista para armazenar os bits do tempo de bit, coloquei so 4 mas pode aumentar e se aumentar tem q aumentar na condição na linha 37
+    i = 0
+
     while True:
-        time.sleep(0.5)
+        time.sleep(3) # Podem ser usado como tempo de bit em vez de receber uma lista de bits pra indicar que o próximo bit é válido
         frame_capture, frame = cap.read()
 
         if not frame_capture:
@@ -26,25 +32,37 @@ def capture():
         cv2.imshow('Frame Original', frame)
         cv2.imshow('Frame Binario', binary_frame)
 
-        
         # Pra pegar o valor que está no centro da imagem
         h, w = binary_frame.shape
         center = binary_frame[h // 2, w // 2]
 
-        
-        if center == 0: 
-            bit = 0
+        if i < 4:
+            # Armazenar os valores de tempo de bit na lista
+            tempo_bit[i] = 1 if center != 0 else 0
+            i += 1
+            print(f"tempo de bit capturado: {list(tempo_bit)}")
         else:
-            bit = 1
+            # Verificação da condição para capturar o bit
+            if tempo_bit == [1, 0, 0, 1]:
+                bits_validos[p] = 1 if center != 0 else 0
+                print(f"Bit válido capturado: {bits_validos[p]}") # Mostra o bit válido capturado da rodada
+                p += 1
+                if p == 8: # Verifica se capturou os 8 bits válidos
+                    print(f"Bits válido capturado: {list(bits_validos)}")
+                    break
 
-        print(f"Bit capturado: {bit}")
+            # Resetar o índice e a lista
+            i = 0
+            tempo_bit = [0, 0, 0, 0]
 
-    
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-    capture()
+    comeco = int(input("Digite 1 pra começar \n")) # Organizar posição de câmera, mas podem remover se acharem desnecessário
+    if comeco == 1:
+        capture()
