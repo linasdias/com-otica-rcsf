96f750da1019748edf895d0d225429a5de78fb14
Paulo Andé <45316554+PauloAndr@users.noreply.github.com>
2024-12-17 20:48:11 -0300

    remoçao do tempo de bit que utilizava 4 bits

--- Captura.py
+++ Captura.py
@@ -1,6 +1,29 @@
+from math import sqrt
 import cv2
-import numpy as np
-import time  # Biblioteca para adicionar a pausa
+import time
+
+
+def isRed(bgr_color) -> bool:
+    blue, green, red = bgr_color
+    # Define um limiar para considerar uma cor como "vermelha"
+    return red > 200 and red > blue and red > green
+
+
+def isGreen(bgr_color) -> bool:
+    blue, green, red = bgr_color
+    # Define um limiar para considerar uma cor como "vermelha"
+    return green > 200 and green > blue and green > red
+
+
+def isBlack(bgr_color, threshold=30):
+    blue, green, red = bgr_color
+    return blue < threshold and green < threshold and red < threshold
+
+
+def isWhite(bgr_color, threshold=225):
+    blue, green, red = bgr_color
+    return blue > threshold and green > threshold and red > threshold
+
 
 def capture():
     cap = cv2.VideoCapture(0)
@@ -9,72 +32,63 @@ def capture():
         print("Erro ao acessar a câmera")
         return
 
-    bits_validos = [0, 0, 0, 0, 0, 0, 0, 0]
+    bits_validos = [0, 0, 0, 0, 0, 0, 0, 0]  # Inicializa a lista para armazenar os bits válidos
     p = 0
-    tempo_bit = [0, 0, 0, 0]
-    i = 0
-    detectou_verde = False
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Erro ao capturar o frame")
+        return
+
+    h, w, _ = frame.shape
+    center = frame[h // 2, w // 2]
+
+    while not isRed(center):
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Erro ao capturar o frame")
+            return
+
+        h, w, _ = frame.shape
+        center = frame[h // 2, w // 2]
 
     while True:
-        cv2.waitKey(1000)
+        time.sleep(1)
         frame_capture, frame = cap.read()
 
         if not frame_capture:
             print("Erro ao capturar o frame")
             break
 
-        # Converte o frame para o espaço de cor HSV
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # Converte o frame para escala de cinza
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Define os intervalos para a cor verde em HSV
-        lower_green = np.array([35, 50, 50])
-        upper_green = np.array([85, 255, 255])
+        # Aplicar um threshold para converter em preto e branco
+        _, binary_frame = cv2.threshold(gray_frame, 225, 255, cv2.THRESH_BINARY)
 
-        # Cria uma máscara para a cor verde
-        green_mask = cv2.inRange(hsv_frame, lower_green, upper_green)
-        cv2.imshow('Máscara Verde', green_mask)
+        # Mostrar o frame original e o frame binarizado
+        # cv2.imshow('Frame Original', frame)
+        # cv2.imshow('Frame Binario', binary_frame)
 
-        # Verifica se a cor verde está presente no centro da imagem
-        h, w = green_mask.shape
-        center_green = green_mask[h // 2, w // 2]
+        # Pra pegar o valor que está no centro da imagem
+        h, w = binary_frame.shape
+        center = binary_frame[h // 2, w // 2]
 
-        if center_green > 0 and not detectou_verde:
-            detectou_verde = True
-            print("Tela verde detectada! Iniciando leitura de bits.")
-            time.sleep(1)  # Pausa de 1 segundo para garantir que o primeiro bit seja mostrado
+        bits_validos[p] = 1 if center != 0 else 0
+        print(f"Bit válido capturado: {bits_validos[p]}")  # Mostra o bit válido capturado da rodada
+        p += 1
+        if p == 8:  # Verifica se capturou os 8 bits válidos
+            print(f"Bits válido capturado: {list(bits_validos)}")
+            break
 
-        if detectou_verde:
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            _, binary_frame = cv2.threshold(gray_frame, 128, 255, cv2.THRESH_BINARY)
-
-            cv2.imshow('Frame Original', frame)
-            cv2.imshow('Frame Binario', binary_frame)
-
-            center = binary_frame[h // 2, w // 2]
-
-            if i < 4:
-                tempo_bit[i] = 1 if center != 0 else 0
-                i += 1
-                print(f"tempo de bit capturado: {list(tempo_bit)}")
-            else:
-                if tempo_bit == [1, 0, 0, 1]:
-                    bits_validos[p] = 1 if center != 0 else 0
-                    print(f"Bit válido capturado: {bits_validos[p]}")
-                    p += 1
-                    if p == 8:
-                        print(f"Bits válidos capturados: {list(bits_validos)}")
-                        break
-                i = 0
-                tempo_bit = [0, 0, 0, 0]
-
-        #if cv2.waitKey(1000) & 0xFF == 27:
-           # break
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
     cap.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    comeco = int(input("Digite 1 para começar \n"))
-    if comeco == 1:
-        capture()
+    capture()
--- transmissor.py
+++ transmissor.py
@@ -12,23 +12,28 @@ def show_bits(bits):
     image = np.zeros((height, width, 3), dtype=np.uint8)
 
     # Mostra a imagem com o temporizador de 5 segundos
-    for i in range(5, 0, -1):
-        countdown_image = image.copy()
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(countdown_image, str(i), (width // 2 - 50, height // 2), font, 10, (255, 255, 255), 10, cv2.LINE_AA)
-        cv2.imshow('Bit Sequence', countdown_image)
-        cv2.waitKey(1000)
+    # for i in range(5, 0, -1):
+    #    countdown_image = image.copy()
+    #    font = cv2.FONT_HERSHEY_SIMPLEX
+    #    cv2.putText(countdown_image, str(i), (width // 2 - 50, height // 2), font, 10, (255, 255, 255), 10, cv2.LINE_AA)
+    #    cv2.imshow('Bit Sequence', countdown_image)
+    #    cv2.waitKey(1000)
 
-    # Exibe a tela verde por 1 segundo
-    green_image = np.zeros((height, width, 3), dtype=np.uint8)
-    green_image[:] = (0, 255, 0)  # Verde
-    cv2.imshow('Bit Sequence', green_image)
+    color = (0, 0, 255)
+
+    image = np.zeros((height, width, 3), dtype=np.uint8)
+    image[:] = color
+
+    # Mostra a imagem na tela
+    cv2.imshow('Start Connection', image)
+    cv2.setWindowProperty('Start Connection', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    # Aguarda 1 segundo
     cv2.waitKey(1000)
 
-    # Adiciona "1001" antes de cada bit na sequência
-    bits_with_prefix = ''.join(["1001" + bit for bit in bits])
+    cv2.destroyAllWindows()
 
-    for bit in bits_with_prefix:
+    for bit in bits:
         # Define a cor: preto (0) ou branco (1)
         color = (255, 255, 255) if bit == '1' else (0, 0, 0)
 
@@ -37,19 +42,25 @@ def show_bits(bits):
         image[:] = color
 
         # Adiciona o bit como texto na tela
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        text_color = (0, 0, 0) if bit == '1' else (255, 255, 255)
-        cv2.putText(image, bit, (width // 2 - 50, height // 2), font, 10, text_color, 10, cv2.LINE_AA)
+        #font = cv2.FONT_HERSHEY_SIMPLEX
+        #text_color = (0, 0, 0) if bit == '1' else (255, 255, 255)
+        #cv2.putText(image, bit, (width // 2 - 50, height // 2), font, 10, text_color, 10, cv2.LINE_AA)
 
         # Mostra a imagem na tela
         cv2.imshow('Bit Sequence', image)
+        cv2.setWindowProperty('Bit Sequence', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
         # Aguarda 1 segundo
         cv2.waitKey(1000)
 
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
 
-# Solicita a sequência de bits do usuário
-bits = input("Digite a sequência de bits: ")
-print(bits)
-show_bits(bits)
+def main():
+    # Solicita a sequência de bits do usuário
+    bits = input("Digite a sequência de bits: ")
+    print(bits)
+    show_bits(bits)
+
+
+if __name__ == "__main__":
+    main()
