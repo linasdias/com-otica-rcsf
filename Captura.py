c419c8546669ae44d84fed2399323480f74164b3
JpSoares17 <jpsoares17@ufpi.edu.br>
2024-12-17 21:40:27 -0300

    Feat: Detecção de tela

--- Captura.py
+++ Captura.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from ultralytics import YOLO
 import cv2
 import time
 
@@ -21,6 +22,7 @@ def isWhite(bgr_color, threshold=225):
     return blue > threshold and green > threshold and red > threshold
 
 def capture():
+    model = YOLO("screen.pt")
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
@@ -38,19 +40,23 @@ def capture():
     if not ret:
         print("Erro ao capturar o frame")
         return
+    
+    classification = 0
 
-    h, w, _ = frame.shape
-    center = frame[h // 2, w // 2]
+    for result in model.predict(frame):
+        for color in result.boxes.cls.to("cpu").numpy().astype(int):
+            classification = color
 
-    while not isRed(center):
+    while result.names[classification] != "Red Screen":
         ret, frame = cap.read()
 
         if not ret:
             print("Erro ao capturar o frame")
             return
-
-        h, w, _ = frame.shape
-        center = frame[h // 2, w // 2]
+        
+        for result in model.predict(frame):
+            for color in result.boxes.cls.to("cpu").numpy().astype(int):
+                classification = color
 
     while True:
         time.sleep(1)
@@ -60,40 +66,19 @@ def capture():
             print("Erro ao capturar o frame")
             break
 
-        # Converte o frame para escala de cinza
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        for result in model.predict(frame):
+            for color in result.boxes.cls.to("cpu").numpy().astype(int):
+                classification = color
 
-        # Aplicar um threshold para converter em preto e branco
-        _, binary_frame = cv2.threshold(gray_frame, 225, 255, cv2.THRESH_BINARY)
+        if result.names[classification] == "White":
+            bits_validos[p] = 1
+            p += 1
+        
+        if result.names[classification] == "Green Screen":
+            bits_validos[p] = 0
+            p += 1
 
-        # Mostrar o frame original e o frame binarizado
-        #cv2.imshow('Frame Original', frame)
-        #cv2.imshow('Frame Binario', binary_frame)
-
-        # Pra pegar o valor que está no centro da imagem
-        h, w = binary_frame.shape
-        center = binary_frame[h // 2, w // 2]
-
-        if i < 4:
-            # Armazenar os valores de tempo de bit na lista
-            tempo_bit[i] = 1 if center != 0 else 0
-            i += 1
-            print(f"tempo de bit capturado: {list(tempo_bit)}")
-        else:
-            # Verificação da condição para capturar o bit
-            if tempo_bit == [1, 0, 0, 1]:
-                bits_validos[p] = 1 if center != 0 else 0
-                print(f"Bit válido capturado: {bits_validos[p]}") # Mostra o bit válido capturado da rodada
-                p += 1
-                if p == 8: # Verifica se capturou os 8 bits válidos
-                    print(f"Bits válido capturado: {list(bits_validos)}")
-                    break
-
-            # Resetar o índice e a lista
-            i = 0
-            tempo_bit = [0, 0, 0, 0]
-
-        if cv2.waitKey(1) & 0xFF == 27:
+        if p == 8:
             break
     
     cap.release()
